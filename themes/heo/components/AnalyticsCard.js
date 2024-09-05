8cd66f9c8b82f174c5703d2b48b94d996887a81a
inthewoods1950 <147363832+inthewoods1950@users.noreply.github.com>
2024-09-05 09:50:22 +0800

    Update AnalyticsCard.js

--- themes/heo/components/AnalyticsCard.js
+++ themes/heo/components/AnalyticsCard.js
@@ -7,7 +7,7 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export function AnalyticsCard(props) {
-  const targetDate = new Date(siteConfig('HEO_SITE_CREATE_TIME', null, CONFIG))
+  const targetDate = new Date('2023-10-12')
   const today = new Date()
   const diffTime = today.getTime() - targetDate.getTime() // 获取两个日期之间的毫秒数差值
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) // 将毫秒数差值转换为天数差值
@@ -21,24 +21,24 @@ export function AnalyticsCard(props) {
                     <div>{postCount}</div>
                 </div>
             </div>
-            //<div className='inline'>
-                //<div className='flex justify-between'>
-                    //<div>建站天数:</div>
-                    //<div>{diffDays} 天</div>
-                //</div>
-            //</div>
-            //<div className='hidden busuanzi_container_page_pv'>
-                //<div className='flex justify-between'>
-                    //<div>访问量:</div>
-                    //<div className='busuanzi_value_page_pv' />
-                //</div>
-            //</div>
-            //<div className='hidden busuanzi_container_site_uv'>
-                //<div className='flex justify-between'>
-                    //<div>访客数:</div>
-                    //<div className='busuanzi_value_site_uv' />
-                //</div>
-            //</div>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>It has been:</div>
+                    <div>{diffDays} Days</div>
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_page_pv'>
+                <div className='flex justify-between'>
+                    <div>Flow:</div>
+                    <div className='busuanzi_value_page_pv' />
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_site_uv'>
+                <div className='flex justify-between'>
+                    <div>Visitors:</div>
+                    <div className='busuanzi_value_site_uv' />
+                </div>
+            </div>
         </div>
         </>
 }
